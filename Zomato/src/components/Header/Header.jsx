fb9d48e57dbd6da4637415027e1629eba90c3a21
Mohammed Akram Uddin <94456647+MohammedAkramU7@users.noreply.github.com>
2024-03-25 15:56:31 +0530

    first commit

---
+++ Zomato/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div>
+        <div className="header">
+            <div className="header-content">
+                <h2>Order your fav food here</h2>
+                <p>order you fav food buddy</p>
+                <button> View More</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
